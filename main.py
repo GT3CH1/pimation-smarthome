2fdae1fdebcbd3534f20e17b7f0d4d81ad9c28e7
gt3ch1 <gcpease@gt3ch1.tk>
2020-12-08 21:47:05 -0700

    Made main switch between different programs

--- main.py
+++ main.py
@@ -4,16 +4,34 @@ from classes import SQLSprinkler as sprinkler
 from classes import TV as tv
 from firebase_admin import credentials, initialize_app, db
 from time import sleep
-cred = credentials.Certificate("resources/serviceAccountKey.json")
+import pathlib
+import sys
+cred = credentials.Certificate(str(pathlib.Path(__file__).parent.absolute()) + "/resources/serviceAccountKey.json")
 initialize_app(cred, {'databaseURL': 'https://new-project-3cddb-default-rtdb.firebaseio.com'})
 time_to_sleep = 3
+tv_first_run = False
 def main():
-    tv.connect()
+    arg = None
+    global tv_first_run
+    if len(sys.argv) != 2:
+        print("Need two arguments!")
+        return
+    else:
+        arg = sys.argv[1]
     while 1:
         ref = db.reference()
-        lp.do_loop(ref)
-        sprinkler.do_loop(ref)
-        tv.do_loop(ref)
+        if arg == "lightpi" or arg == "all":
+            lp.do_loop(ref)
+        if arg == "sprinkler" or arg == "all":
+            sprinkler.do_loop(ref)
+
+        if((arg == "tv" or arg == "all") and tv.checkTvOnOff(ref)):
+            if not tv_first_run:
+                tv.connect()
+                tv_first_run = True
+                tv.do_loop(ref)
+            else:
+                tv.do_loop(ref)
         sleep(time_to_sleep)
 if __name__ == "__main__":
     main()
