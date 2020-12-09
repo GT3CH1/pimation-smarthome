adb585bae0d9c36dd7f5c8e917956c4b0d97d6a8
gt3ch1 <gcpease@gt3ch1.tk>
2020-12-08 21:46:50 -0700

    Made TV only turn on once.

--- classes/TV.py
+++ classes/TV.py
@@ -3,9 +3,11 @@ from pywebostv.connection import *
 from pywebostv.controls import *
 from wakeonlan import send_magic_packet
 import json
+from time import sleep
+from os import system
 from os import getcwd
 from os import environ
-
+import pathlib
 # 1. For the first run, pass in an empty dictionary object. Empty store leads to an Authentication prompt on TV.
 # 2. Go through the registration process. `store` gets populated in the process.
 # 3. Persist the `store` state to disk.
@@ -14,6 +16,7 @@ store = {'client_key' : environ.get("CLIENT_KEY")}
 ip = None
 mac = None
 first_run = True 
+last_mute = True
 last_power = True
 last_vol = -1
 
@@ -23,6 +26,40 @@ system_client = None
 # you could skip the slow scan and # instead simply say:
 #    client = WebOSClient("<IP Address of TV>")
 
+def update_values(ref, power, volume, muted):
+    stepsize = ref.get()['upstairs-tv']['Volume']['stepSize']
+    print(" power: {0}, volume: {1}, muted: {2} ".format(power,volume,muted))
+    data = {'upstairs-tv': { 'OnOff':{'on': power}, 'Volume': {'currentVolume': volume, 'stepSize': stepsize, 'isMuted': muted}}}
+    ref.update(data)
+
+def checkTvOnOff(ref):
+    global last_power
+    global mac
+    global ip
+    data = ref.get()['upstairs-tv']
+    if (ip == None and mac == None):
+        with open(str(pathlib.Path(__file__).parent.parent.absolute()) + '/resources/tv.json') as file: 
+            clientdata = json.load(file)
+            ip = clientdata['tv']['ip']
+            mac = clientdata['tv']['mac']
+
+    # pings the tv to see if it is on or off
+    if(system('/usr/bin/ping '+ip+' -c 1 -w 1 > /dev/null ') == 0):
+        last_power = True
+    volume = ref.get()['upstairs-tv']['Volume']['currentVolume']
+    muted = ref.get()['upstairs-tv']['Volume']['isMuted']
+        # If the TV is off and the status in firebase is on, turn it on.
+    if(data['OnOff']['on']):
+        last_power = True
+        send_magic_packet(mac, ip_address=ip)
+        update_values(ref, False, volume, muted)
+        #else:
+         #   print("tv is suppoed to be off.")
+            # The tv is suppoed to be off.
+         #   last_power = False
+         #   if not last_power:
+         #       update_values(ref, False, volume, muted)
+       
 def connect():
     global first_run
     global media_client
@@ -30,56 +67,56 @@ def connect():
     global store
     global ip
     global mac
-    ip = None
-    with open(getcwd() + '/resources/tv.json') as file:
-        clientdata = json.load(file)
-        ip = clientdata['tv']['ip']
-        mac = clientdata['tv']['mac']
     if first_run:
         client = WebOSClient(ip)
         client.connect()
-        print("Key : " + str(store))
         register = client.register(store)
         for status in register:
-                print(status)
                 if status == 2:
                     first_run = True
         media_client = MediaControl(client)
         system_client = SystemControl(client)
-        print("Setting up media client.")
 
 def update_settings(ref):
     global last_vol
     global media_client
     current_tv_vol = media_client.get_volume()
-    firebase_vol = ref.get()['upstairs-tv']['Volume']['currentVolume']
-    if(current_tv_vol != firebase_vol):
-        data = {'upstairs-tv': { 'Volume': {'currentVolume': current_tv_vol}}}
-        ref.update(data)
-        print("updating tv volume")
+    current_mute_state = current_tv_vol['muted']
+    firebase_vol = ref.get()['upstairs-tv']['Volume']
+    if (current_tv_vol['volume'] != firebase_vol['currentVolume']) or (firebase_vol['isMuted'] != current_mute_state):
+        print("Updating firebase.")
+        update_values(ref,True,current_tv_vol['volume'],current_mute_state)
+        return
 
 def do_loop(ref):
     global first_run
     global last_vol
+    global last_mute
     global last_power
     global media_client
     global system_client
     global mac
     data = ref.get()['upstairs-tv']
     vol = data['Volume']['currentVolume']
-    #power = data['OnOff']['on']
-    if vol != last_vol:
-        print("Changing vol.")
+    isMuted = data['Volume']['isMuted']
+    power = data['OnOff']['on']
+    if (vol != last_vol) or (isMuted != last_mute):
         first_run = False
         last_vol = vol
+        last_mute = isMuted
         media_client.set_volume(vol) 
-    #if power != last_power:
-    #    print("Changing power.")
-    #    last_power = power
-    #    if not power:
-    #        system_client.power_off()
+        media_client.mute(isMuted)
+    # Check the power status
+    if power != last_power:
+        last_power = power
+        # If firebase power is off, turn it off.
+        if not power and last_power:
+            try:
+                system_client.power_off()
+            except Exception:
+                return
+            return
     update_settings(ref)
 
 # Keep the 'store' object because it contains now the access token
 # and use it next time you want to register on the TV.
-print(store)  # {'client_key': 'ACCESS_TOKEN_FROM_TV'}
