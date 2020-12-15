65cb7cf18d52d965323e4c9fd3fb394956445d56
gt3ch1 <gcpease@gt3ch1.tk>
2020-12-14 17:29:26 -0700

    Working on multiple argument: Added garage module to loop Added basement router module to loop

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 from classes import LightPi as lp
 from classes import SQLSprinkler as sprinkler
-from classes import TV as tv
+from classes import TV as lgtv
+from classes import Garage as garage
+from classes import BasementRouter as br
 from firebase_admin import credentials, initialize_app, db
+
 from time import sleep
+
 import pathlib
 import sys
+import getopt
+
 cred = credentials.Certificate(str(pathlib.Path(__file__).parent.absolute()) + "/resources/serviceAccountKey.json")
 initialize_app(cred, {'databaseURL': 'https://new-project-3cddb-default-rtdb.firebaseio.com'})
-time_to_sleep = 3
-tv_first_run = False
+time_to_sleep = 1
+lgtv_first_run = True
 def main():
     arg = None
-    global tv_first_run
+    lgtv_first_run = True
     if len(sys.argv) != 2:
         print("Need two arguments!")
         return
@@ -20,18 +26,21 @@ def main():
         arg = sys.argv[1]
     while 1:
         ref = db.reference()
+        if arg == "garage" or arg == "all":
+            garage.do_loop(ref)
+        if arg == "br" or arg == "all":
+            br.do_loop(ref)
         if arg == "lightpi" or arg == "all":
             lp.do_loop(ref)
         if arg == "sprinkler" or arg == "all":
             sprinkler.do_loop(ref)
-
-        if((arg == "tv" or arg == "all") and tv.checkTvOnOff(ref)):
-            if not tv_first_run:
-                tv.connect()
-                tv_first_run = True
-                tv.do_loop(ref)
+        if((arg == "tv" or arg == "all") and lgtv.checkTvOnOff(ref)):
+            if lgtv_first_run:
+                lgtv.connect()
+                lgtv_first_run = False
+                lgtv.do_loop(ref)
             else:
-                tv.do_loop(ref)
+                lgtv.do_loop(ref)
         sleep(time_to_sleep)
 if __name__ == "__main__":
     main()
