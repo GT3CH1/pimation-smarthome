bab85896b322ad880e056fceb47c9e510f60a2e7
gt3ch1 <gcpease@gt3ch1.tk>
2020-12-14 17:27:48 -0700

    use https ; print

--- classes/LightPi.py
+++ classes/LightPi.py
@@ -1,5 +1,5 @@
 import requests
-LIGHT_URL = "http://lightpi.peasenet.com/"
+LIGHT_URL = "https://lightpi.peasenet.com/"
 PREV_ON = False
 def do_loop(ref):
          global LIGHT_URL
@@ -10,5 +10,6 @@ def do_loop(ref):
                 params = {'status': 'on'}
             else:
                 params = {'status': 'off'}
+            print("Turning {0} rock garden lights".format("on" if on else "off"))
             requests.get(url=LIGHT_URL + "submit.php", params=params)
             PREV_ON =on
