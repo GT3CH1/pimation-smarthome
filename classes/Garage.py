4b5c1722c90eb0672ac2d2821eabedb185db1bfb
GT3CH1 <gavinpease@gmail.com>
2021-05-27 14:11:18 -0600

    Clean up garage door - make remote false immediately.

--- classes/Garage.py
+++ classes/Garage.py
@@ -1,13 +1,30 @@
 import requests
+
 GARAGE_URL = "https://pimation.peasenet.com/modules/garage/"
 PREV_OPEN = 0
+FIRST_RUN = True
+garage_data = {
+    'garage-door':
+        {
+            'OpenClose': {
+                'openPercent': 0,
+                'remote': False
+            }
+        }
+}
+
+
 def do_loop(ref):
-         global GARAGE_URL
-         global PREV_OPEN
-         curropen = ref.get()['garage-door']['OpenClose']['openPercent']
-         if curropen != PREV_OPEN:
-            print("Toggling garage...")
-            params = {'toggle': '1'} 
-            requests.post(url=GARAGE_URL + "submit.php", data=params)
-            PREV_OPEN = curropen
-            print("PREV_OPEN sat properly: {0}".format(PREV_OPEN == curropen)) 
+    global GARAGE_URL
+    global PREV_OPEN
+    global FIRST_RUN
+    if FIRST_RUN:
+        ref.update(garage_data)
+        FIRST_RUN = False
+    is_remote = ref['garage-door']['OpenClose']['remote']
+    if is_remote:
+        print("Toggling garage...")
+        params = {'toggle': '1'}
+        requests.post(url=GARAGE_URL + "submit.php", data=params)
+        garage_data['garage-door']['OpenClose']['remote'] = False
+        ref.update(garage_data)
